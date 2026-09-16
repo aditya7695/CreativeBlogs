d61ea69369111b83706a295eac3fdae28e42af55
aditya7695 <ubaleaditya96@gmail.com>
2026-09-16 10:27:07 +0530

    Implemented password hashing

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,27 @@
+import User from "../models/user";
+import bcrypt from "bcrypt"
+
 export const register = async(req,res) => {
     try {
+
+        const {name , email , password} = req.body;
+
+        if(!name || !email || !password){
+            return res.status(404).json({
+                success : false,
+                message : "You have to provide all 3 details to register" 
+            })
+        }
+
+        const userFromDb = await User.findOne({email});
+
+        if(userFromDb) {
+            return res.status(400).json({
+                success : false,
+                message : "User already exist"
+            })
+        }
+
         
     } catch (error) {
         return res.status(400).json({
